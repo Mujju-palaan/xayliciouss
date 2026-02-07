b12302cf718c6801d727f2f74cbec352b16ee686
Mujju-palaan <mdmujahid97@gmail.com>
2026-02-07 14:24:13 +0530

    changes

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <div className="container bg-white 500 rounded-xl
+        text-center text-4xl sm:text-6xl font-extrabold text-[#333333]">
+        XAYLICIOUSS
+      </div>
+      
+    </div>
+  );
+}
